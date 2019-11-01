ef1778ab2def3e25c3a0d7562149c6febe91d321
RaghuBN <raghu.astepahead@gmail.com>
2019-11-01 21:33:48 +0530

    added ReadMe.txt

---
+++ src/jenkinsDemo/TestMethod.java
@@ -0,0 +1,12 @@
+package jenkinsDemo;
+
+public class TestMethod 
+{
+	public static void main(String[] args) 
+	{
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println("Welcome To Jenkins With Git...... "  + i);
+		}
+	}
+}
